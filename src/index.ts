721595671e1849c8eee2893c08d200ba1b768c84
ChakshuGautam <chaks.gautam@gmail.com>
2023-06-09 18:36:58 +0530

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import crypto from 'crypto';
+
+interface JsonLdDocument {
+  '@context': string;
+  '@type': string;
+  [key: string]: any;
+}
+
+interface Consent {
+  attribute: string;
+  limit: number;
+  currentUsage: number;
+}
+
+class ZKP {
+  public document: JsonLdDocument;
+  public attribute: string;
+  private hash: string;
+  private value: any;
+  private consent: Consent;
+
+  constructor(document: JsonLdDocument, attribute: string, consent: Consent) {
+    this.document = document;
+    this.attribute = attribute;
+    this.consent = consent;
+    if (this.document[this.attribute]) {
+      this.hash = crypto
+        .createHash('sha256')
+        .update(this.document[this.attribute].toString())
+        .digest('hex');
+      this.value = this.document[this.attribute];
+    } else {
+      throw new Error('Attribute not found in the document');
+    }
+    if (this.consent.attribute !== attribute) {
+      throw new Error('Consent attribute does not match document attribute');
+    }
+  }
+
+  checkConsent(): boolean {
+    return this.consent.currentUsage < this.consent.limit;
+  }
+
+  incrementUsage(): void {
+    if (this.checkConsent()) {
+      this.consent.currentUsage++;
+    } else {
+      throw new Error('API usage limit reached');
+    }
+  }
+
+  checkAttributeExists(proverAttribute: string): boolean {
+    this.incrementUsage();
+    const proverHash = crypto
+      .createHash('sha256')
+      .update(proverAttribute)
+      .digest('hex');
+    return proverHash === this.hash;
+  }
+
+  checkAttributeGreaterThan(value: number): boolean {
+    this.incrementUsage();
+    return this.value > value;
+  }
+
+  checkAttributeLessThan(value: number): boolean {
+    this.incrementUsage();
+    return this.value < value;
+  }
+
+  checkAttributeEqualTo(value: any): boolean {
+    this.incrementUsage();
+    return this.value === value;
+  }
+
+  checkAttributeInRange(min: number, max: number): boolean {
+    this.incrementUsage();
+    return this.value >= min && this.value <= max;
+  }
+}
+
+export { ZKP, JsonLdDocument, Consent };
---
+++ test/blah.test.ts
@@ -0,0 +1,60 @@
+import { ZKP, JsonLdDocument, Consent } from '../src';
+
+describe('ZKP Class', () => {
+  let jsonLdDocument: JsonLdDocument;
+  let consent: Consent;
+
+  beforeEach(() => {
+    jsonLdDocument = {
+      '@context': 'http://schema.org',
+      '@type': 'Person',
+      name: 'John Doe',
+      age: 25,
+    };
+
+    consent = {
+      attribute: 'age',
+      limit: 5,
+      currentUsage: 0,
+    };
+  });
+
+  it('should initialize without errors', () => {
+    const jsonLdDocument = {
+      '@context': 'http://schema.org',
+      '@type': 'Person',
+      name: 'John Doe',
+      age: 25,
+    };
+
+    const consent = {
+      attribute: 'age',
+      limit: 5,
+      currentUsage: 0,
+    };
+    const zkp = new ZKP(jsonLdDocument, 'age', consent);
+    expect(zkp).toBeInstanceOf(ZKP);
+  });
+
+  it('should checkAttributeExists correctly', () => {
+    const zkp = new ZKP(jsonLdDocument, 'age', consent);
+    expect(zkp.checkAttributeExists('25')).toBe(true);
+    expect(zkp.checkAttributeExists('26')).toBe(false);
+  });
+
+  it('should checkAttributeGreaterThan correctly', () => {
+    const zkp = new ZKP(jsonLdDocument, 'age', consent);
+    expect(zkp.checkAttributeGreaterThan(20)).toBe(true);
+    expect(zkp.checkAttributeGreaterThan(30)).toBe(false);
+  });
+
+  it('should throw error if usage limit is exceeded', () => {
+    const zkp = new ZKP(jsonLdDocument, 'age', consent);
+    for (let i = 0; i < 5; i++) {
+      zkp.checkAttributeGreaterThan(20);
+    }
+    expect(() => zkp.checkAttributeGreaterThan(20)).toThrow(
+      'API usage limit reached'
+    );
+  });
+});
